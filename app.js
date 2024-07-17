674c7e3048176e8260a033b3ede7f11c219961ff
unknown <danbuitrago87@gmail.com>
2024-07-17 16:39:58 -0500

    JS Game con lista de numeros generados

---
+++ app.js
@@ -0,0 +1,83 @@
+//DEFINIR NUMERO ALEATORIO A TRAVES DE LA FUNCION GENERADORA
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+
+//FUNCIÓN PARA ASIGNAR TEXTOS A LOS ELEMENTOS DE HTML
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+//FUNCION PETICIÓN AL USUARIO DE NÚMERO
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log("Número de Usuario: " +numeroDeUsuario);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        console.log("intentos: " + intentos);
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        if (intentos==3){
+            asignarTextoElemento('p','FALLASTE');
+            document.getElementById('reiniciar').removeAttribute('disabled');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+//FUNCION PARA LIMPIAR CAJA
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+//FUNCION GENERADORA DE NUMERO ALEATORIO
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log("Numero generado: " +numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+//FUNCION PARA VOLVER A LAS CONDICIONES INICIALES
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+//FUNCION PARA REINICIAR JUEGO
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
